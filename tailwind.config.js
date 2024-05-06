6351a098db74b73964e990465c82952395f9eed8
AceReX <ace.rexnoir@gmail.com>
2024-05-06 13:10:56 +0530

    add custom css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
+import plugin from "tailwindcss/plugin";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: "selector",
+  plugins: [
+    // eslint-disable-next-line no-undef
+    require("@tailwindcss/typography"),
+    plugin(function ({ addComponents }) {
+      addComponents({
+        ".main-layout-rows": {
+          gridTemplateRows: "80px minmax(500px,1fr) auto",
+        },
+        ".main-layout-col": {
+          gridTemplateColumns: "1fr 2fr",
+        },
+        ".forecast-layout-row": {
+          gridTemplateRows: "20px clamp(80px, 500px, 1fr)",
+        },
+        ".forecast-items-container-col": {
+          gridTemplateColumns: "repeat(auto-fill, minmax(250px, 1fr))",
+        },
+        ".header-columns": {
+          gridTemplateColumns: "2fr 1fr",
+        },
+      });
+    }),
+  ],
 };
